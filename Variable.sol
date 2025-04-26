6aa065a1dca5f364170e876fc1b0866af8bddcf8
jaiganesh2108 <jaig7335@gmail.com>
2025-04-26 20:37:23 +0530

    first commit

---
+++ Variable.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^8.0.8;
+
+contract Practice{
+    int public integerVar = -10;
+    int public intRangeMax = type(int).max;
+    int public intRangeMin = type(int).min; 
+    uint public unsignedIntegerVariable = 10;
+    address myAddress = 0x5B38Da6a701c568545dCfcB03f56beddC4;
+    bool status = true;
+    string myname = "jaiganesh";
+    byte myNameInByte = "jaiganesh";
+}
